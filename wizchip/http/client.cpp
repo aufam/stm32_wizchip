19eae47e1da697d3fc7f16f75d8627d1e7e25cd6
aufam <aufamuhammad715@gmail.com>
2023-12-11 22:01:46 +0700

    adding client

---
+++ wizchip/http/client.cpp
@@ -0,0 +1,100 @@
+#include "Ethernet/socket.h"
+#include "wizchip/http/client.h"
+#include "etl/string.h"
+
+using namespace Project;
+using namespace Project::wizchip;
+
+int http::Client::on_init(int) {
+    return SOCK_OK;
+}
+
+int http::Client::on_listen(int) {
+    return SOCK_OK;
+}
+
+int http::Client::on_established(int socket_number) {
+    // Interrupt clear
+    if (getSn_IR(socket_number) & Sn_IR_CON)
+        setSn_IR(socket_number, Sn_IR_CON);
+
+    // send request
+    this->process(socket_number, Ethernet::txData);
+
+    // Check the TX socket buffer for End of HTTP response sends
+    while (getSn_TX_FSR(socket_number) != (getSn_TxMAX(socket_number))) {}
+
+    // receive response
+    size_t len = getSn_RX_RSR(socket_number);
+    
+    if (len > WIZCHIP_BUFFER_LENGTH) 
+        len = WIZCHIP_BUFFER_LENGTH;
+
+    len = ::recv(socket_number, Ethernet::rxData, len);
+    Ethernet::rxData[len] = '\0';
+
+    _response = Response::parse(Ethernet::rxData, len);
+    if (handler) {
+        handler(_response);
+    } else {
+        event.setFlags(1);
+    }
+    _is_running = false;
+
+    return SOCK_OK;
+}
+
+int http::Client::on_close_wait(int socket_number) {
+    return ::disconnect(socket_number);
+}
+
+int http::Client::on_closed(int socket_number) {
+    if (not _is_running) {
+        deallocate(socket_number);
+        return SOCK_OK;
+    }
+    auto ip = host.split<4>(".");
+    uint8_t addr[] = { (uint8_t) ip[0].to_int(), (uint8_t) ip[1].to_int(), (uint8_t) ip[2].to_int(), (uint8_t) ip[3].to_int() };
+    ::socket(socket_number, Sn_MR_TCP, port, Sn_MR_ND);
+    return ::connect(socket_number, addr, port);
+}
+
+auto http::Client::request(const http::Request& req) -> http::Response {
+    _is_running = true;
+    _request = req;
+    this->handler = nullptr;
+    allocate(1);
+
+    event.init();
+    event.resetFlags(1);
+
+    if (event.waitFlagsAny({.timeout=_timeout})) {
+        return _response;
+    } else {
+        return {};
+    }
+}
+
+void http::Client::request(const http::Request& req, HandlerFunction handler) {
+    _is_running = true;
+    _request = req;
+    this->handler = handler;
+    allocate(1);
+}
+
+void http::Client::process(int socket_number, uint8_t* txBuffer) {
+    auto &text = etl::string_cast<WIZCHIP_BUFFER_LENGTH>(txBuffer);
+    text(
+        "%.*s %.*s %.*s" "\r\n"
+        "%.*s" "\r\n\r\n"
+        "%.*s",
+        _request.method.len(), _request.method.begin(), 
+        _request.url.len(), _request.url.begin(),
+        _request.version.len(), _request.version.begin(),
+        _request.head.len(), _request.head.begin(),
+        _request.body.len(), _request.body.begin()
+    );
+
+    // send
+    ::send(socket_number, Ethernet::txData, text.len());
+}
---
+++ wizchip/http/client.h
@@ -0,0 +1,113 @@
+#ifndef WIZCHIP_HTTP_CLIENT_H
+#define WIZCHIP_HTTP_CLIENT_H
+
+#include "etl/function.h"
+#include "etl/event.h"
+#include "wizchip/ethernet.h"
+#include "wizchip/http/request.h"
+#include "wizchip/http/response.h"
+
+namespace Project::wizchip::http {
+
+    class Client : public Socket {
+    public:
+        struct Args {
+            Ethernet& ethernet;
+            etl::StringView host;
+            int port;
+        };
+
+        constexpr explicit Client(Args args) : Socket({args.ethernet, args.port}), host(args.host) {}
+
+        using HandlerFunction = etl::Function<void(const Response&), void*>;
+
+        Response request(const Request& req);
+        void request(const Request& req, HandlerFunction handler);
+
+        Response Get(etl::StringView url) { 
+            return request({.method="GET", .url=url, .version="HTTP/1.1", .head="", .body=""}); 
+        }
+
+        Response Put(etl::StringView url, etl::StringView head, etl::StringView body) {
+            return request({.method="PUT", .url=url, .version="HTTP/1.1", .head=head, .body=body}); 
+        }
+
+        Response Post(etl::StringView url, etl::StringView head, etl::StringView body) {
+            return request({.method="POST", .url=url, .version="HTTP/1.1", .head=head, .body=body}); 
+        }
+
+        Response Patch(etl::StringView url, etl::StringView head, etl::StringView body) {
+            return request({.method="PATCH", .url=url, .version="HTTP/1.1", .head=head, .body=body}); 
+        }
+        
+        Response Head(etl::StringView url) {
+            return request({.method="HEAD", .url=url, .version="HTTP/1.1", .head="", .body=""}); 
+        }
+
+        Response Trace(etl::StringView url, etl::StringView head, etl::StringView body) {
+            return request({.method="TRACE", .url=url, .version="HTTP/1.1", .head=head, .body=body}); 
+        }
+
+        Response Delete(etl::StringView url, etl::StringView head = "", etl::StringView body = "") {
+            return request({.method="DELETE", .url=url, .version="HTTP/1.1", .head=head, .body=body}); 
+        }
+
+        Response Options(etl::StringView url) {
+            return request({.method="OPTIONS", .url=url, .version="HTTP/1.1", .head="", .body=""}); 
+        }
+
+        void Get(etl::StringView url, HandlerFunction handler) { 
+            request({.method="GET", .url=url, .version="HTTP/1.1", .head="", .body=""}, handler); 
+        }
+
+        void Put(etl::StringView url, etl::StringView head, etl::StringView body, HandlerFunction handler) {
+            request({.method="PUT", .url=url, .version="HTTP/1.1", .head=head, .body=body}, handler); 
+        }
+
+        void Post(etl::StringView url, etl::StringView head, etl::StringView body, HandlerFunction handler) {
+            request({.method="POST", .url=url, .version="HTTP/1.1", .head=head, .body=body}, handler); 
+        }
+
+        void Patch(etl::StringView url, etl::StringView head, etl::StringView body, HandlerFunction handler) {
+            request({.method="PATCH", .url=url, .version="HTTP/1.1", .head=head, .body=body}, handler); 
+        }
+        
+        void Head(etl::StringView url, HandlerFunction handler) {
+            request({.method="HEAD", .url=url, .version="HTTP/1.1", .head="", .body=""}, handler); 
+        }
+
+        void Trace(etl::StringView url, etl::StringView head, etl::StringView body, HandlerFunction handler) {
+            request({.method="TRACE", .url=url, .version="HTTP/1.1", .head=head, .body=body}, handler); 
+        }
+
+        void Delete(etl::StringView url, etl::StringView head, etl::StringView body, HandlerFunction handler) {
+            request({.method="DELETE", .url=url, .version="HTTP/1.1", .head=head, .body=body}, handler); 
+        }
+
+        void Options(etl::StringView url, HandlerFunction handler) {
+            request({.method="OPTIONS", .url=url, .version="HTTP/1.1", .head="", .body=""}, handler); 
+        }
+
+        etl::Function<void(const Request&, const Response&), void*> debug;
+
+        /// default timeout = 1s;
+        etl::Time _timeout = etl::time::seconds(1);
+    
+    protected:
+        int on_init(int socket_number) override;
+        int on_listen(int socket_number) override;
+        int on_established(int socket_number) override;
+        int on_close_wait(int socket_number) override;
+        int on_closed(int socket_number) override;
+        void process(int socket_number, uint8_t* txBuffer);
+
+        etl::StringView host;
+        HandlerFunction handler;
+        etl::Event event;
+        Request _request = {};
+        Response _response = {};
+        bool _is_running = false;
+    };
+}
+
+#endif
